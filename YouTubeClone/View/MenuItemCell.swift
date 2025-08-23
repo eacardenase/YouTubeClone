6d138a232c59ad258d2eac8f53830bba546e9d38
Edwin Cardenas <ecardenas@lwolf.com>
2025-08-22 22:32:26 -0500

    refactor: use compositional layout for menu item cell

--- YouTubeClone/View/MenuItemCell.swift
+++ YouTubeClone/View/MenuItemCell.swift
@@ -44,11 +44,45 @@ class MenuItemCell: UICollectionViewCell {
         return layout
     }()
 
+    let compositionalLayout: UICollectionViewCompositionalLayout = {
+        let fraction: CGFloat = 1 / 3
+
+        // Item
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(fraction),
+            heightDimension: .fractionalHeight(1)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let inset: CGFloat = 0.5
+
+        item.contentInsets = NSDirectionalEdgeInsets(
+            top: inset,
+            leading: inset,
+            bottom: inset,
+            trailing: inset
+        )
+
+        // Group
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1),
+            heightDimension: .fractionalWidth(0.5)
+        )
+        let group = NSCollectionLayoutGroup.horizontal(
+            layoutSize: groupSize,
+            subitems: [item]
+        )
+
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+
+        return UICollectionViewCompositionalLayout(section: section)
+    }()
+
     lazy var collectionView: UICollectionView = {
 
         let _collectionView = UICollectionView(
             frame: .zero,
-            collectionViewLayout: self.flowLayout
+            collectionViewLayout: self.compositionalLayout
         )
 
         _collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -126,27 +160,6 @@ extension MenuItemCell: UICollectionViewDataSource {
         cell.backgroundColor = contentItems[indexPath.item]
 
         return cell
-    }
-
-}
-
-// MARK: - UICollectionViewDelegateFlowLayout
-
-extension MenuItemCell: UICollectionViewDelegateFlowLayout {
-
-    func collectionView(
-        _ collectionView: UICollectionView,
-        layout collectionViewLayout: UICollectionViewLayout,
-        sizeForItemAt indexPath: IndexPath
-    ) -> CGSize {
-        let numberOfItemsPerRow: CGFloat = 3
-        let spacing: CGFloat = flowLayout.minimumInteritemSpacing
-
-        let width = collectionView.bounds.width
-        let availableWidth = width - spacing * (numberOfItemsPerRow + 1)
-        let itemDimension = floor(availableWidth / numberOfItemsPerRow)
-
-        return CGSize(width: itemDimension, height: itemDimension * 1.5)
     }
 
 }
