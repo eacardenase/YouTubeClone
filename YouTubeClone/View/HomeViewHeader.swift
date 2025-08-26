3fb911532fa1e97f7b1aa5650f170ab353fd9c22
Edwin Cardenas <ecardenas@lwolf.com>
2025-08-25 21:11:17 -0500

    feat: add subscription and community buttons

--- YouTubeClone/View/HomeViewHeader.swift
+++ YouTubeClone/View/HomeViewHeader.swift
@@ -29,19 +29,29 @@ class HomeViewHeader: UIView {
         let label = UILabel()
 
         let attributedText = NSMutableAttributedString(
-            string:
-                "Videos that teach 8-bit hardware and software hacking by way of the original Nintendo Entertainment System. ",
+            string: """
+                Videos that teach 8-bit hardware and software hacking \
+                by way of the original Nintendo Entertainment System. 
+                """,
             attributes: [
-                .font: UIFont.preferredFont(forTextStyle: .footnote),
+                .font: UIFont.preferredFont(forTextStyle: .caption1),
                 .foregroundColor: UIColor.secondaryLabel,
             ]
         )
+
+        guard
+            let fontDescriptor: UIFontDescriptor = .preferredFontDescriptor(
+                withTextStyle: .caption1
+            ).withSymbolicTraits(.traitBold)
+        else {
+            fatalError("Could not create font descriptor with bold trait")
+        }
 
         attributedText.append(
             NSAttributedString(
                 string: "...more",
                 attributes: [
-                    .font: UIFont.preferredFont(forTextStyle: .footnote),
+                    .font: UIFont(descriptor: fontDescriptor, size: 0),
                     .foregroundColor: UIColor.label,
                 ]
             )
@@ -52,6 +62,68 @@ class HomeViewHeader: UIView {
         label.lineBreakMode = .byWordWrapping
 
         return label
+    }()
+
+    let subscribeButton: UIButton = {
+        let button = UIButton(type: .custom)
+
+        var config = UIButton.Configuration.gray()
+        config.cornerStyle = .capsule
+
+        button.configuration = config
+
+        guard
+            let fontDescriptor: UIFontDescriptor = .preferredFontDescriptor(
+                withTextStyle: .footnote
+            ).withSymbolicTraits(.traitBold)
+        else {
+            fatalError("Could not create font descriptor with bold trait")
+        }
+
+        let attributedText = NSAttributedString(
+            string: "Subscribed",
+            attributes: [
+                .font: UIFont(descriptor: fontDescriptor, size: 0),
+                .foregroundColor: UIColor.label,
+            ]
+        )
+
+        button.setAttributedTitle(attributedText, for: .normal)
+
+        return button
+    }()
+
+    let communityButton: UIButton = {
+        let button = UIButton(type: .custom)
+
+        var config = UIButton.Configuration.gray()
+        config.cornerStyle = .capsule
+
+        button.configuration = config
+
+        guard
+            let fontDescriptor: UIFontDescriptor = .preferredFontDescriptor(
+                withTextStyle: .footnote
+            ).withSymbolicTraits(.traitBold)
+        else {
+            fatalError("Could not create font descriptor with bold trait")
+        }
+
+        let attributedText = NSAttributedString(
+            string: "Visit Community",
+            attributes: [
+                .font: UIFont(descriptor: fontDescriptor, size: 0),
+                .foregroundColor: UIColor.label,
+            ]
+        )
+
+        button.setAttributedTitle(attributedText, for: .normal)
+        button.tintColor = .label
+        button.titleLabel?.font = .preferredFont(
+            forTextStyle: .extraLargeTitle2
+        )
+
+        return button
     }()
 
     // MARK: Initializers
@@ -75,15 +147,25 @@ extension HomeViewHeader {
     private func setupViews() {
         translatesAutoresizingMaskIntoConstraints = false
 
+        let buttonStack = UIStackView(arrangedSubviews: [
+            subscribeButton,
+            communityButton,
+        ])
+
+        buttonStack.axis = .horizontal
+        buttonStack.distribution = .fillEqually
+        buttonStack.spacing = 8
+
         let stackView = UIStackView(arrangedSubviews: [
             channelBanner,
             profileView,
             profileDescription,
+            buttonStack,
         ])
 
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
-        stackView.spacing = 16
+        stackView.spacing = 12
 
         addSubview(stackView)
 
@@ -92,7 +174,7 @@ extension HomeViewHeader {
             stackView.topAnchor.constraint(equalTo: topAnchor),
             stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
             stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
         ])
     }
 
